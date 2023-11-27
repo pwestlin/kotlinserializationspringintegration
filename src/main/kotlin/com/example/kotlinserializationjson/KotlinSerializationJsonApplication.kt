a0e734dbef0c1a0dccb800ea05f7b2fa562478e5
petves <petves@lm.se>
2023-11-27 18:45:15 +0100

    Trix

--- src/main/kotlin/com/example/kotlinserializationjson/KotlinSerializationJsonApplication.kt
+++ src/main/kotlin/com/example/kotlinserializationjson/KotlinSerializationJsonApplication.kt
@@ -7,6 +7,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -27,10 +28,10 @@ class CarsController {
     private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
 
     private val vehicles: ArrayList<Vehicle> = arrayListOf(
-                Car(name = "Vovvon", brand = "Volvo", model = "V16", year = 2019),
-                Car(name = "Fårrden", brand = "Ford", model = "Mondeo", year = 2007),
-                Motorcycle(name = "Enduron", brand = "KTM", model = "EXC-F 350", year = 2018),
-            )
+        Car(id = 1, name = "Vovvon", brand = "Volvo", model = "V16", year = 2019),
+        Car(id = 2, name = "Fårrden", brand = "Ford", model = "Mondeo", year = 2007),
+        Motorcycle(id = 3, name = "Enduron", brand = "KTM", model = "EXC-F 350", year = 2018),
+    )
 
     @GetMapping("/all")
     fun all(): List<Vehicle> {
@@ -41,27 +42,37 @@ class CarsController {
     }
 
     @PostMapping("")
-    fun saveNew(@RequestBody vehicle: Vehicle) {
+    suspend fun saveNew(@RequestBody vehicle: Vehicle): ResponseEntity<Unit> {
         logger.info("body: $vehicle")
-        vehicles.add(vehicle)
+        return if (vehicles.none { it.id == vehicle.id }) {
+            vehicles.add(vehicle)
+            ResponseEntity.ok().build()
+        } else {
+            ResponseEntity.badRequest().build()
+        }
     }
 }
 
 @Serializable
 sealed interface Vehicle {
+    val id: Int
     val name: String
 }
 
 @Serializable
 data class Car(
+    override val id: Int,
+    override val name: String,
     val brand: String,
     val model: String,
-    val year: Int, override val name: String
-): Vehicle
+    val year: Int
+) : Vehicle
 
 @Serializable
 data class Motorcycle(
+    override val id: Int,
+    override val name: String,
     val brand: String,
     val model: String,
-    val year: Int, override val name: String
-): Vehicle
+    val year: Int
+) : Vehicle
