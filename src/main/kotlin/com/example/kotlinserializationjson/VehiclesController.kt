2db3b14d0b90fb6649dc27e4466d09229282183b
petves <petves@lm.se>
2023-11-28 17:42:19 +0100

    Cleaning

--- src/main/kotlin/com/example/kotlinserializationjson/VehiclesController.kt
+++ src/main/kotlin/com/example/kotlinserializationjson/VehiclesController.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
